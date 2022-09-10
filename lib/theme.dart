9141f5e4471bd57d22b08accabbb5f6147f1d910
TedoHarisC <tedoharischandra29@gmail.com>
2022-09-10 16:09:47 +0700

    Menambahkan Theme dan Asset untuk aplikasi

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color kPrimaryColor = const Color(0xffC74E5B);
-Color kSecondaryColor = const Color(0xffD9AC7F);
+Color kPrimaryColor = const Color(0xffF05A22);
+Color kSecondaryColor = const Color(0xffF9DDCF);
+Color kOrangeColor = const Color(0xffF87B4C);
+Color kGreyColor = const Color(0xff898989);
+Color kBlackColor = const Color(0xff000000);
+Color kWhiteColor = const Color(0xffFFFFFF);
+
 Color kGreenColor = const Color(0xff24C690);
 Color kRedColor = const Color(0xffFF576B);
 
@@ -21,6 +26,22 @@ TextStyle primaryTextStyle = GoogleFonts.nunito(
 
 TextStyle secondaryTextStyle = GoogleFonts.nunito(
   color: kSecondaryColor,
+);
+
+TextStyle orangeTextStyle = GoogleFonts.nunito(
+  color: kOrangeColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.nunito(
+  color: kOrangeColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.nunito(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color: kWhiteColor,
 );
 
 TextStyle greenTextStyle = GoogleFonts.nunito(
